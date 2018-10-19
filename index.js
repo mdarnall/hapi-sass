ac49d2adaafd4925151ab636cb1e064382e4dba5
Matt Darnall <mdarnall@salesforce.com>
2018-10-19 16:29:02 -0700

    Updates for Hapi17
    - Adds prettier
    - Updates package version
    - updates code to newer node/js

--- index.js
+++ index.js
@@ -1,171 +1,172 @@
-'use strict'
+"use strict";
 
-var Boom = require('boom'),
-    sass = require('node-sass'),
-    Hoek = require('hoek'),
-    fs = require('fs'),
-    dirname = require('path').dirname,
-    mkdirp = require('mkdirp'),
-    join = require('path').join;
+const Boom = require("boom"),
+  sass = require("node-sass"),
+  Hoek = require("hoek"),
+  fs = require("fs"),
+  dirname = require("path").dirname,
+  mkdirp = require("mkdirp"),
+  join = require("path").join;
 
-var internals = {
+const { promisify } = require("util");
 
-    defaults: {
-        /* https://github.com/sass/node-sass#options */
-        debug: false,
-        force: false,
-        src: './lib/sass',
-        dest: './public/css',
-        routePath: '/css/{file}.css',
-        outputStyle: 'compressed',
-        sourceComments: 'none',
-        srcExtension: 'scss'
-    },
+const sassRenderAsync = promisify(sass.render);
+const writeFileAsync = promisify(fs.writeFile);
+const stat = promisify(fs.stat);
 
-    error: function (reply, err) {
-        if (err.code == 'ENOENT') {
-            return reply(Boom.notFound());
-        }
-        else {
-            return reply(Boom.internal(err));
-        }
-    },
+const internals = {
+  defaults: {
+    /* https://github.com/sass/node-sass#options */
+    debug: false,
+    force: false,
+    src: "./lib/sass",
+    dest: "./public/css",
+    routePath: "/css/{file}.css",
+    outputStyle: "compressed",
+    sourceComments: "none",
+    srcExtension: "scss"
+  },
 
-    log: function () {
-        var args = Array.prototype.slice.call(arguments);
-        args[0] = '[hapi-sass] ' + args[0];
-        console.log.apply(console, args)
+  error: function(h, err) {
+    if (err.code === "ENOENT") {
+      return Boom.notFound();
+    } else {
+      return Boom.internal(err);
     }
+  },
+
+  log: function() {
+    const args = Array.prototype.slice.call(arguments);
+    args[0] = "[hapi-sass] " + args[0];
+    console.log.apply(console, args);
+  }
 };
 
-exports.register = function (server, options, next) {
-
-    var settings = Hoek.applyToDefaults(internals.defaults, options);
+exports.plugin = {
+  register: (server, options) => {
+    const settings = Hoek.applyToDefaults(internals.defaults, options);
     // Force compilation
-    var force = settings.force;
+    const force = settings.force;
 
     // Debug option
-    var debug = settings.debug;
+    const debug = settings.debug;
 
     // Source dir required
-    var src = settings.src;
+    const src = settings.src;
     if (!src) {
-        next(new Boom('hapi-sass requires "src" directory'));
+      return Boom.badRequest('hapi-sass requires "src" directory');
     }
 
     // Default dest dir to source
-    var dest = settings.dest ? settings.dest : src;
+    const dest = settings.dest ? settings.dest : src;
 
     server.route({
-        method: 'GET',
-        path: settings.routePath,
-        config: { files: { relativeTo: './' } },
-        handler: function (request, reply) {
-
-            var cssPath = join(dest, request.params.file + '.css'),
-                sassPath = join(src, request.params.file + '.' + settings.srcExtension),
-                sassDir = dirname(sassPath);
-
-            if (debug) {
-                internals.log("Processing Request with values: %j", {
-                    "sassPath": sassPath,
-                    "dest": dest,
-                    "sassDir": sassDir
-                })
-            }
-
-            var compile = function () {
-
-                if (debug) {
-                    internals.log('Compiling Sass at %s', sassPath);
-                }
-
-                sass.render({
-                    file: sassPath,
-                    includePaths: [sassDir].concat(settings.includePaths || []),
-                    imagePath: settings.imagePath,
-                    outputStyle: settings.outputStyle,
-                    functions: settings.functions,
-                    sourceComments: settings.sourceComments
-                }, function (err, result) {
-
-                    if (err) {
-                        if (debug) {
-                            let message = err.formatted ? err.formatted : err.message;
-                            internals.log('Compilation failed: %s', message);
-                        }
-                        return internals.error(reply, err);
-                    }
-
-                    if (debug) {
-                        internals.log('Compilation ok');
-                    }
-
-                    mkdirp(dirname(cssPath), 0x1c0, function (err) {
-                        if (err) {
-                            return reply(err);
-                        }
-                        fs.writeFile(cssPath, result.css, 'utf8', function (err) {
-
-                            if(err && debug){
-                                internals.log("Error writing file - %s", err.message);
-                            }
-                            reply(result.css).type('text/css');
-                        });
-                    });
-                });
-            };
-
-            if (force) {
-                return compile();
-            }
-
-
-            fs.stat(sassPath, function (err, sassStats) {
-
-                if (err) {
-                    return internals.error(reply, err);
-                }
-                fs.stat(cssPath, function (err, cssStats) {
-
-                    if (err) {
-                        if (err.code == 'ENOENT') {
-                            // css has not been compiled
-                            if (debug) {
-                                internals.log('Compiled file not found, compiling %s', cssPath);
-                            }
-                            compile();
-
-                        } else {
-                            internals.error(reply, err);
-                        }
-                    }
-                    else { // compiled version exists, check mtimes
-
-                        if (sassStats.mtime.getTime() > cssStats.mtime.getTime()) { // the sass version is newer
-                            if (debug) {
-                                internals.log('Sass file is newer, compiling %s', cssPath);
-                            }
-                            compile();
-                        }
-                        else {
-                            // serve
-                            if (debug) {
-                                internals.log('Compiled file found and up to date. Serving');
-                            }
-                            reply.file(cssPath);
-                        }
-                    }
-                });
-            });
+      method: "GET",
+      path: settings.routePath,
+      config: {
+        files: {
+          relativeTo: "./"
         }
+      },
+      handler: async function(request, h) {
+        const cssPath = join(dest, request.params.file + ".css"),
+          sassPath = join(
+            src,
+            request.params.file + "." + settings.srcExtension
+          ),
+          sassDir = dirname(sassPath);
+
+        if (debug) {
+          internals.log("Processing Request with values: %j", {
+            sassPath: sassPath,
+            dest: dest,
+            sassDir: sassDir
+          });
+        }
+
+        const compile = async () => {
+          if (debug) {
+            internals.log("Compiling Sass at %s", sassPath);
+          }
+
+          const results = await sassRenderAsync({
+            file: sassPath,
+            includePaths: [sassDir].concat(settings.includePaths || []),
+            imagePath: settings.imagePath,
+            outputStyle: settings.outputStyle,
+            functions: settings.functions,
+            sourceComments: settings.sourceComments
+          });
+
+          let err = results.err;
+          if (err) {
+            if (debug) {
+              let message = err.formatted ? err.formatted : err.message;
+              internals.log("Compilation failed: %s", message);
+            }
+            return internals.error(h, err);
+          }
+
+          if (debug) {
+            internals.log("Compilation ok");
+          }
+
+          let errMk = await mkdirp(dirname(cssPath), 0x1c0);
+          if (errMk) {
+            return errMk;
+          }
+          let errFs = await writeFileAsync(cssPath, results.css, "utf8");
+
+          if (errFs && debug) {
+            internals.log("Error writing file - %s", errFs.message);
+          }
+          return h.response(results.css).type("text/css");
+        };
+
+        if (force) {
+          return compile();
+        }
+
+        let sassStats, cssStats;
+        try {
+          sassStats = await stat(sassPath);
+        } catch (err) {
+          return internals.error(h, err);
+        }
+
+        try {
+          cssStats = await stat(cssPath);
+        } catch (err) {
+          if (err.code === "ENOENT") {
+            if (debug) {
+              internals.log("Compiled file not found, compiling %s", cssPath);
+            }
+            return compile();
+          } else {
+            internals.error(h, err);
+          }
+        }
+
+        // compiled version exists, check mtimes
+        if (sassStats.mtime.getTime() > cssStats.mtime.getTime()) {
+          // the sass version is newer
+          if (debug) {
+            internals.log("Sass file is newer, compiling %s", cssPath);
+          }
+          return compile();
+        } else {
+          // serve
+          if (debug) {
+            internals.log("Compiled file found and up to date. Serving");
+          }
+          return h.file(cssPath);
+        }
+      }
     });
-
-    next();
-};
-
-exports.register.attributes = {
-    multiple: true,
-    dependencies: 'inert',
-    pkg: require('./package.json')
+  },
+  multiple: true,
+  dependencies: "inert",
+  name: "hapi-sass",
+  pkg: require("./package.json"),
+  version: require("./package.json").version
 };
--- test/indexTest.js
+++ test/indexTest.js
@@ -1,25 +1,28 @@
-var HapiSass = require('../index');
-var expect = require('chai').expect;
-var Hapi = require('hapi');
+const HapiSass = require('../index')
+const Inert = require('inert')
+const expect = require('chai').expect
+const Hapi = require('hapi')
 
-describe('Hapi-Sass', function () {
+describe("Hapi-Sass", () => {
+  describe("register", () => {
+    it("creates a route for the plugin", async () => {
+      const server = new Hapi.Server();
+      const err = await server.register([
+        Inert,
+        {
+          plugin: HapiSass,
+          options: {}
+        }
+      ])
 
-    describe('register', function () {
+      expect(err).to.not.exist;
+      await server.start();
+      const tables = server.table()
+      expect(tables.length).to.be.greaterThan(0);
 
-        it('creates a route for the plugin', function (done) {
-            var server = new Hapi.Server();
-            server.connection();
-            server.register({ register: HapiSass, options: {}}, function (err) {
-                    expect(err).to.not.exist;
-                    var tables = server.table();
-                    expect(tables.length).to.be.greaterThan(0);
-                    var routes = tables[0].table;
-                    expect(routes.length).to.be.greaterThan(0);
-                    expect(routes[0].path).to.equal('/css/{file}.css');
-                    done();
-                }
-            );
-        });
+      const route = tables[0].path
+      expect(route).to.exist;
+      expect(route).to.equal("/css/{file}.css");
     });
-
+  });
 });
